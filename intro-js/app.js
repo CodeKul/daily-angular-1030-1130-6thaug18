3b690c3f49b96009b1058295c4e387f8c18749c1
aniruddha <aniruddha.kudalkar@gmail.com>
2018-08-07 11:17:13 +0530

    first commit

---
+++ intro-js/app.js
@@ -0,0 +1,38 @@
+console.log('Welcome to codekul')
+
+var nm = 'android'
+nm = 45
+
+function whatsNm() {
+    console.log('Name is anything')
+}
+
+whatsNm()
+
+let dt = 20
+if(true) {
+    let dt = 50
+}
+console.log('Value of data is - '+ dt)
+console.log(`
+    Value of data is - ${dt}
+    sddndslkgn
+    slkggksalkg
+    lkssdjglksgj
+    skgklskgj
+`)
+
+function welcome(gsts, csts) {
+    return gsts + csts
+}   
+welcome(10, 56)
+
+
+
+
+
+
+
+
+
+
---
+++ intro/app.js
@@ -0,0 +1,9 @@
+// var ipNm = document.getElementById('ipNm')
+// ipNm.onkeyup = function () {
+//     var dvDt = document.getElementById('dvDt')
+//     dvDt.innerHTML = ipNm.value
+// }
+
+function chIt(dvDt, dt) {
+    dvDt.innerHTML = dt
+}
