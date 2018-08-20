9aba985d89b1573bfbc918071a20bc845257ff3f
aniruddha <aniruddha.kudalkar@gmail.com>
2018-08-20 11:04:23 +0530

    project structure : understanding

--- angular/mysample/src/app/app.module.ts
+++ angular/mysample/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginModule } from './login/login.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
