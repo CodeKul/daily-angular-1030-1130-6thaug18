1403d99affa9839b9b825da0f804c19613a49a3d
aniruddha <aniruddha.kudalkar@gmail.com>
2018-09-05 10:41:43 +0530

    directives : custom attribute directive

--- angular/mysample/src/app/directives/magic.directive.ts
+++ angular/mysample/src/app/directives/magic.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appMagic]'
 })
 export class MagicDirective {
 
+  @Input('appMagic')
   @HostBinding('style.background')
   hstClr: string
 
