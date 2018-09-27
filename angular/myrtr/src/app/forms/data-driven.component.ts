07bac9d8bee3d9f6c9cc2d88e89d9875a77f473d
aniruddha <aniruddha.kudalkar@gmail.com>
2018-09-27 11:37:28 +0530

    forms : data driven

--- angular/myrtr/src/app/forms/data-driven.component.ts
+++ angular/myrtr/src/app/forms/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -7,23 +7,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./data-driven.component.css']
 })
 export class DataDrivenComponent implements OnInit {
-
+ // https://bootsnipp.com/snippets/featured/message-chat-box
   fg: FormGroup
 
   constructor(
     private fb: FormBuilder
-  ) { }
+  ) {
+   }
 
   ngOnInit() {
     this.fg = this.fb.group({
-      nm: this.fb.control('', Validators.required),
+      nm: this.fb.control('', Validators.required, this.sampleAsync),
       eml: this.fb.control('', Validators.compose([Validators.required, Validators.email])),
-      mbl: this.fb.control('', Validators.required)
+      mbl: this.fb.control('', Validators.compose([Validators.required, this.tenDigitsMob]))
     })
   }
 
   frmSub() {
     console.log(this.fg)
     console.log(this.fg.value)
+  }
+
+  tenDigitsMob(control: AbstractControl): ValidationErrors | null {
+    console.log(control.value.length)
+    if (control.value.length > 10) return { length: control.value.length }
+    return null
+  }
+
+  sampleAsync(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise<ValidationErrors | null>(
+      (res, rej) => setTimeout(
+        () => {
+          if (control.value.charAt(0) == 'a') res(null)
+          else res({ nota: control.value.charAt(0) })
+        }, 2500
+      )
+    )
   }
 }
